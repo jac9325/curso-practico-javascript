45fd214ab0750664bf6556d869795059478cea52
Your Name <you@example.com>
2021-10-03 17:48:10 -0500

    agregue 2 archivos uno es figuras.html y figuras.js para el primer taller

---
+++ figuras.js
@@ -0,0 +1,7 @@
+const lado = 5;
+console.log("el lado del cuadrado es: "+lado);
+var perimetro = lado*4;
+console.log("El perimetro del cuadrado es: "+perimetro);
+
+var area = lado*lado;
+console.log("El area del cuadrado es: "+area);
