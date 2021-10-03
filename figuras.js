2a5277666de0a91b4bb63ea58198f52b989155e4
Your Name <you@example.com>
2021-10-03 18:22:42 -0500

    modifique ambos archivos para el taller 2

--- figuras.js
+++ figuras.js
@@ -1,7 +1,11 @@
-const lado = 5;
-console.log("el lado del cuadrado es: "+lado);
-var perimetro = lado*4;
-console.log("El perimetro del cuadrado es: "+perimetro);
 
-var area = lado*lado;
-console.log("El area del cuadrado es: "+area);
+function perimetro(){
+    const input = document.getElementById("imputCuadrado").value;
+    alert("el perimetro del cuadrado es: "+input*4);
+    
+};
+
+function area(lado){
+    return lado*lado;
+};
+
